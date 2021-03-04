b56ffad3fbe029dc6430530827251615d848bc1f
naomihutauruk08 <80033041+naomihutauruk08@users.noreply.github.com>
2021-03-04 16:07:25 +0700

    membuat program nengetahui angka positif, negatif dan angka 0

---
+++ index.cpp
@@ -0,0 +1,16 @@
+  #include <iostream>
+  using namespace std;
+  
+  int main() {
+  int a;
+  cout << "Masukkan angka : ";
+  cin>> a;
+  if (a > 0) {
+  cout << "bilangan positif ";
+  }else if(a == 0) {
+  cout << "ini adalah 0;
+  }else {
+  cout << "bilangan negatif";
+  }
+  return 0;
+  }
